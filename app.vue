d671bfd9d1f36f5abfa0bc5dbec4c2a9620028af
Phan Hong Nam (TrippleN) <phan.hong.nam@azoom.jp>
2022-11-29 11:35:08 +0700

    :tada: Initial project

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const myName = ref("");
+</script>
+
+<template>
+  <div>
+    <app-input v-model="myName" label="Nam" />
+    <span>{{ myName }}</span>
+  </div>
+</template>
---
+++ components/app-input.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface Props {
+  modelValue: string | number;
+  label?: string;
+}
+
+const { modelValue, label } = defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | number): void;
+}>();
+
+const onInput = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+
+  emit("update:modelValue", input.value);
+};
+</script>
+
+<template>
+  <div class="app-input" role="input">
+    <label v-if="label">{{ label }}</label>
+    <input type="text" :value="modelValue" @input="onInput" />
+  </div>
+</template>
